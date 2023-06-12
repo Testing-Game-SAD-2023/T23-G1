b7ce1fc9cc2e57eca126cc074609f71220c5f92e
doriana-99 <73281947+doriana-99@users.noreply.github.com>
2023-06-12 10:03:54 +0200

    Delete src directory

--- src/main/resources/static/link.js
+++
@@ -1,7 +0,0 @@
-const confermaLink = document.getElementById('conferma-link');
-const confermatoMsg = document.getElementById('confermato-msg');
-
-confermaLink.addEventListener('click', function(event) {
-  event.preventDefault();
-  confermatoMsg.classList.remove('hidden');
-});
