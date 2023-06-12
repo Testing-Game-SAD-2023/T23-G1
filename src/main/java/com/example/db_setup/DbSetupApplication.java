d2a3eb1196118260969c98c2db05876316a1aad7
doriana-99 <73281947+doriana-99@users.noreply.github.com>
2023-06-12 10:33:46 +0200

    Surce prog

---
+++ src/main/java/com/example/db_setup/DbSetupApplication.java
@@ -0,0 +1,12 @@
+package com.example.db_setup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbSetupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbSetupApplication.class, args);
+    }
+}
